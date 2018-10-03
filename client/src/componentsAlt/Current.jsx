16862fd529f61c8a29c9b46a55cb7dfa63b63a9a
Vladislav <vit3@rutgers.edu>
2018-10-03 11:03:28 -0600

    minor updates

--- client/src/componentsAlt/Current.jsx
+++ client/src/componentsAlt/Current.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
 // Components 
 import Dashboard from './Dashboard'
@@ -8,26 +8,20 @@ import CurrentAction from './CurrentAction'
 import Activity from './Activity'
 import Stats from './Stats'
 
-class Current extends Component {
-    constructor(){
-        super()
-
-    }
-    render() {
-        return (
-            <div id="main-content-full">
-                <div id="main-content-wrapper">
-                    <Switch>
-                        <Route exact path="/" component={Dashboard}/>
-                        <Route exact path="/current" component={CurrentAction}/>
-                        <Route exact path="/activity" component={Activity}/>
-                        <Route exact path="/history" component={History}/>
-                        <Route exact path="/statistics" component={Stats}/>
-                    </Switch>
-                </div>
+const Current = () => {
+    return (
+        <div id="main-content-full">
+            <div id="main-content-wrapper">
+                <Switch>
+                    <Route exact path="/" component={Dashboard}/>
+                    <Route exact path="/current" component={CurrentAction}/>
+                    <Route exact path="/activity" component={Activity}/>
+                    <Route exact path="/history" component={History}/>
+                    <Route exact path="/statistics" component={Stats}/>
+                </Switch>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-export default Current
+export default withRouter(Current)
--- client/src/componentsAlt/Stats.jsx
+++ client/src/componentsAlt/Stats.jsx
@@ -5,18 +5,12 @@ import {Bar, Line, Pie} from 'react-chartjs-2'
 class Stats extends Component {
     constructor(){
         super()
-        this.state = {
-            chartData: {
-                
-            }
-        }
     }
     render(){
         return (
             <div id="">
                 <h1 className="page-title">statistics</h1>
                 <Bar
-                    data={data}
                     width={100}
                     height={50}
                     options={{
